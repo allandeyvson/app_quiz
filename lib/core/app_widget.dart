1afe463522af3c5df55cd98f055ac55cee31de79
allandeyvson <deyvson7@gmail.com>
2021-06-29 21:57:16 -0300

    first commit

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:app_quiz/home/home_page.dart';
+import 'package:app_quiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "App Quiz",
+      home: HomePage(),
+    );
+  }
+}
